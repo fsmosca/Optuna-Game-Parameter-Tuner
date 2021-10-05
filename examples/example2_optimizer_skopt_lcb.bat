0d0096731ead27cbe3e31d1d1be11667bbb48de8
fsmosca <fsmosca@users.noreply.github.com>
2021-10-05 23:34:17 +0800

    Update example2_optimizer_skopt_lcb.bat

--- examples/example2_optimizer_skopt_lcb.bat
+++ examples/example2_optimizer_skopt_lcb.bat
@@ -1,4 +1,5 @@
 :: Example 2
+:: Usage, if you want to use this bat file, copy this file to the location of tuner.py.
 
 :: --sampler name=skopt acquisition_function=LCB
 
@@ -7,4 +8,4 @@
 
 
 
-python tuner.py --study-name sf_d1_skopt_LCB --sampler name=skopt acquisition_function=LCB --fix-base-param --engine ./engines/stockfish-modern/stockfish.exe --hash 64 --concurrency 1 --opening-file ./start_opening/ogpt_chess_startpos.epd --opening-format epd --input-param "{'eMobilityBonus[2][10]': {'default':158, 'min':100, 'max':200, 'step':4}, 'mOutpost[0]': {'default':56, 'min':0, 'max':100, 'step':4}, 'eThreatByMinor[4]': {'default':119, 'min':50, 'max':150, 'step':4}, 'mThreatBySafePawn': {'default':173, 'min':120, 'max':220, 'step':4}}" --games-per-trial 1000 --trials 100 --plot --base-time-sec 60 --inc-time-sec 0.1 --depth 1 --pgn-output train_sf_d1.pgn --threshold-pruner result=0.35
+python tuner.py --study-name sf_d1_skopt_LCB --sampler name=skopt acquisition_function=LCB --engine ./engines/stockfish-modern/stockfish.exe --concurrency 1 --opening-file ./start_opening/ogpt_chess_startpos.epd --opening-format epd --input-param "{'eMobilityBonus[2][10]': {'default':158, 'min':100, 'max':200, 'step':2}, 'mOutpost[0]': {'default':56, 'min':0, 'max':100, 'step':4}, 'eThreatByMinor[4]': {'default':119, 'min':50, 'max':150, 'step':1}, 'mThreatBySafePawn': {'default':173, 'min':120, 'max':220, 'step':1}}" --games-per-trial 1000 --trials 100 --plot --depth 1 --pgn-output train_sf_d1_skopt_LCB.pgn --threshold-pruner result=0.35
