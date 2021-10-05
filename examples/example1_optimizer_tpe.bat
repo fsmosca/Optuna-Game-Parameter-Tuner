941cba801f05febd2bb7fea8e89cfb1d446a50f9
fsmosca <fsmosca@users.noreply.github.com>
2021-10-05 23:32:11 +0800

    Update example1_optimizer_tpe.bat

--- examples/example1_optimizer_tpe.bat
+++ examples/example1_optimizer_tpe.bat
@@ -11,10 +11,6 @@
 :: --sampler name=tpe
 :: The optimizer is TPE.
 
-:: --fix-base-param
-:: The best param is the default param and is always used by the base_engine.
-:: test_engine vs base_engine, and test_engine will use param values from optimizer.
-
 :: --engine ./engines/stockfish-modern/stockfish.exe
 :: The path and filename of engine.
 
@@ -50,4 +46,4 @@
 :: the match will continue for another 500 games to complete the games per trial requirement of 1000.
 
 
-python tuner.py --study-name sf_d1 --sampler name=tpe --fix-base-param --engine ./engines/stockfish-modern/stockfish.exe --hash 64 --concurrency 6 --opening-file ./start_opening/ogpt_chess_startpos.epd --opening-format epd --input-param "{'eMobilityBonus[2][10]': {'default':158, 'min':100, 'max':200, 'step':4}, 'mOutpost[0]': {'default':56, 'min':0, 'max':100, 'step':4}, 'eThreatByMinor[4]': {'default':119, 'min':50, 'max':150, 'step':4}, 'mThreatBySafePawn': {'default':173, 'min':120, 'max':220, 'step':4}}" --games-per-trial 1000 --trials 200 --plot --base-time-sec 60 --inc-time-sec 0.1 --depth 1 --pgn-output train_sf_d1.pgn --threshold-pruner result=0.35
+python tuner.py --study-name sf_d1 --sampler name=tpe --engine ./engines/stockfish-modern/stockfish.exe --concurrency 6 --opening-file ./start_opening/ogpt_chess_startpos.epd --opening-format epd --input-param "{'eMobilityBonus[2][10]': {'default':158, 'min':100, 'max':200, 'step':2}, 'mOutpost[0]': {'default':56, 'min':0, 'max':100, 'step':4}, 'eThreatByMinor[4]': {'default':119, 'min':50, 'max':150, 'step':1}, 'mThreatBySafePawn': {'default':173, 'min':120, 'max':220, 'step':1}}" --games-per-trial 1000 --trials 100 --plot --depth 1 --pgn-output train_sf_d1.pgn --threshold-pruner result=0.35
